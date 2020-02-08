64da8544d8e02b7366b6524cedbafa03e8e732bc
sanket2197 <sanket9721kumar@gmail.com>
2020-02-08 18:21:17 +0530

    Added Nodemon/Created First Get API

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API /");
+});
+
+app.get("/get-users", (req, res) => {
+  res.send("GET USERS");
+});
+
+app.get("/get-blogs", (req, res) => {
+  res.send("GET BLOGS");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`));
